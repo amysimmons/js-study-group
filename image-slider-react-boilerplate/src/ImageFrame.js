cfa1510db60dc671938da11729c5d7c1ced90d99
amysimmons <amy__simmons@hotmail.com>
2015-09-26 16:21:25 +1000

    slider in progress

--- image-slider-react-boilerplate/src/ImageFrame.js
+++ image-slider-react-boilerplate/src/ImageFrame.js
@@ -3,10 +3,15 @@ import React from 'react';
 var ImageFrame = React.createClass({
 	render: function(){
 		var images = this.props.images;
+		var currentImage = this.props.currentImage;
+		var nextImage = this.props.nextImage;
+
+		var imageToDisplay = images[currentImage];
+
 		return (
 			<div className="image-frame">
 				<div className="image">
-					{images}
+					{imageToDisplay}
 				</div>
 			</div>
 		)
--- image-slider-react-boilerplate/src/Slider.js
+++ image-slider-react-boilerplate/src/Slider.js
@@ -6,15 +6,23 @@ import ImageSelectors from './ImageSelectors';
 
 var Slider = React.createClass({
 	getInitialState: function(){
+		var imageUrls = [
+				"http://images7.alphacoders.com/311/311271.jpg",
+				"http://7-themes.com/data_images/out/73/7022411-water-whale-art.jpg",
+				"https://s3-eu-west-1.amazonaws.com/3tags-prod/tag/542d448574cfc/542d44857c54c/original.jpg"
+			]
+
+		var images = []
+
+		for (var i = 0; i < imageUrls.length; i++) {
+			var imageUrl = imageUrls[i];
+			images.push(<img className={i} src={imageUrl}/>)
+		};
+
 		return{
-			images: [
-				<img src="http://images7.alphacoders.com/311/311271.jpg"/>,
-				<img src="http://7-themes.com/data_images/out/73/7022411-water-whale-art.jpg"/>,
-				<img src="https://s3-eu-west-1.amazonaws.com/3tags-prod/tag/542d448574cfc/542d44857c54c/original.jpg"/>
-			],
+			images: images,
 			currentImage: 0,
-			previousImage: null,
-			nextImage: null,
+			nextImage: null
 		};
 	},
 	moveLeft: function(){
@@ -31,23 +39,31 @@ var Slider = React.createClass({
 		var currentImage = this.state.currentImage;
 		var nextImage = this.state.nextImage;
 
-		nextImage = currentImage ++;
-
-		if (nextImage > images.length -1){
+		if (nextImage >= images.length-1){
 			nextImage = 0;
+		}
+		else {
+			nextImage = currentImage + 1;
 		}
 
 		currentImage = nextImage;
-		this.state.render;
+		this.setState({currentImage: currentImage, nextImage: nextImage});
 	},
 	selectImage: function(){
 
 	},
 	render: function(){
+		var images = this.state.images,
+			currentImage = this.state.currentImage,
+			nextImage = this.state.nextImage
+
 		return (
 			<div className="slider">
-				<ImageFrame images={this.state.images}/>
-				<ArrowRightFrame moveRight={this.moveRight}/>
+				<ImageFrame images={images}
+				currentImage={currentImage}
+				nextImage={nextImage}/>
+				<ArrowRightFrame 
+					moveRight={this.moveRight}/>
 				<ArrowLeftFrame moveLeft={this.moveLeft}/>
 				<ImageSelectors selectSide={this.selectImage}/>
 			</div>
