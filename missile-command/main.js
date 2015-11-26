c0112be2a66ef5e69e541fb97aac405c36a7f92c
amysimmons <amy__simmons@hotmail.com>
2015-11-26 15:11:25 +1100

    refactoring to enable instances of friendly and enemy missile objects

--- missile-command/main.js
+++ missile-command/main.js
@@ -1,78 +1,93 @@
-getRandomPoint = function(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+App = {
+	enemyMissile: function() {
+		this.startPosX = Math.floor(Math.random() * (500 - 0 + 1)) + 0,
+		this.startPosY = 0,
+		this.endPosX = Math.floor(Math.random() * (500 - 0 + 1)) + 0,
+		this.endPosY = 500,
+		this.active = true,
+		this.color = 'red',
+		this.explosionZone= {}
+	},
+
+	playerMissile: function() {
+		this.startPosX = Math.floor(Math.random() * (500 - 0 + 1)) + 0,
+		this.startPosY = 0,
+		this.endPosX = Math.floor(Math.random() * (500 - 0 + 1)) + 0,
+		this.endPosY = 500,
+		this.active = true,
+		this.color = 'blue',
+		this.explosionZone = {}
+	},
+
+	generateEnemyMissile: function(){
+		var enemyMissile = new App.enemyMissile();
+		App.launchEnemyMissile(enemyMissile);
+	},
+
+	launchEnemyMissile: function(enemyMissile){
+		var canvas = document.getElementById("canvas");
+		var ctx = canvas.getContext("2d");
+
+		// variable to hold how many frames have elapsed in the animation
+		var t = 1;
+
+		// define the path to plot
+		var vertices = [];
+		vertices.push({
+		    x: enemyMissile.startPosX,
+		    y: enemyMissile.startPosY
+		});
+		vertices.push({
+		    x: enemyMissile.endPosX,
+		    y: enemyMissile.endPosY
+		});
+
+		var points = calcWaypoints(vertices);
+		console.log(points)
+		// extend the line from start to finish with animation
+		animate(points);
+
+		// calc waypoints traveling along vertices
+		function calcWaypoints(vertices) {
+		    var waypoints = [];
+		    for (var i = 1; i < vertices.length; i++) {
+		        var pt0 = vertices[i - 1];
+		        var pt1 = vertices[i];
+		        var dx = pt1.x - pt0.x;
+		        var dy = pt1.y - pt0.y;
+		        for (var j = 0; j < 100; j++) {
+		            var x = pt0.x + dx * j / 100;
+		            var y = pt0.y + dy * j / 100;
+		            waypoints.push({
+		                x: x,
+		                y: y
+		            });
+		        }
+		    }
+		    return (waypoints);
+		}
+
+		function animate() {
+		    if (t < points.length - 1) {
+		    	//slows down the animation 
+		    	var framesPerSecond = 10;
+			   	setTimeout(function(){
+		    		requestAnimationFrame(animate);
+		    	}, 1000 / framesPerSecond);
+		    }
+		    // draw a line segment from the last waypoint
+		    // to the current waypoint
+		    ctx.beginPath();
+		    ctx.moveTo(points[t - 1].x, points[t - 1].y);
+		    ctx.lineTo(points[t].x, points[t].y);
+		    ctx.stroke();
+		    // increment "t" to get the next waypoint
+		    t++;
+		}
+
+	},
+
 }
 
-var startPosX = getRandomPoint(0, 500);
-var startPosY = 0;
-var nextPosX = startPosX++;
-var nextPosY = startPosY++;
-var endPosX = getRandomPoint(0, 500);
-var endPosY = 500;
-
-var missile = {
-	startPosX: startPosX,
-	startPosY: startPosY,
-	currentPosX: startPosX,
-	currentPosY: startPosY,
-	endPosX: endPosX,
-	endPosY: endPosY,
-	active: true,
-	color: 'red',
-}
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-ctx.lineCap = "round";
-
-// variable to hold how many frames have elapsed in the animation
-var t = 1;
-
-// define the path to plot
-var vertices = [];
-vertices.push({
-    x: startPosX,
-    y: startPosY
-});
-vertices.push({
-    x: endPosX,
-    y: endPosY
-});
-
-var points = calcWaypoints(vertices);
-// extend the line from start to finish with animation
-animate(points);
-
-// calc waypoints traveling along vertices
-function calcWaypoints(vertices) {
-    var waypoints = [];
-    for (var i = 1; i < vertices.length; i++) {
-        var pt0 = vertices[i - 1];
-        var pt1 = vertices[i];
-        var dx = pt1.x - pt0.x;
-        var dy = pt1.y - pt0.y;
-        for (var j = 0; j < 100; j++) {
-            var x = pt0.x + dx * j / 100;
-            var y = pt0.y + dy * j / 100;
-            waypoints.push({
-                x: x,
-                y: y
-            });
-        }
-    }
-    return (waypoints);
-}
-
-function animate() {
-    if (t < points.length - 1) {
-        requestAnimationFrame(animate);
-    }
-    // draw a line segment from the last waypoint
-    // to the current waypoint
-    ctx.beginPath();
-    ctx.moveTo(points[t - 1].x, points[t - 1].y);
-    ctx.lineTo(points[t].x, points[t].y);
-    ctx.stroke();
-    // increment "t" to get the next waypoint
-    t++;
-}
+App.generateEnemyMissile();
 
